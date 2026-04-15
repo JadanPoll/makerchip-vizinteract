64267919f412352ebe9e488dba778441b45fd8e3
JadanPoll <99365944+JadanPoll@users.noreply.github.com>
2026-04-14 22:24:21 -0500

    Create cxxrtl_bare_baremetal.h for bare-metal use
    Add a bare-metal header for minimal code functionality without C Standard library.

---
+++ cxxrtl_bare_baremetal.h
@@ -0,0 +1,3 @@
+// A bare-metal port that includes the absolute minimal stuff for code to work. DOES NOT USE C Standard library, debug and $print simulation statements removed
+// only with -nostdlib
+// Notes: Instaniates 16MB for potential RAM use(maximum wasm limit). There are probably tons of optimizations in here
